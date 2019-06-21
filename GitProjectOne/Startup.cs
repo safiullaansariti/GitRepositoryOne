7a11b11c4af103fbeaeb8441135a396abcbfb2d6
Admin <Admin@ADMIN-PC>
2019-06-21 11:05:31 +0530

    Add project files.

---
+++ GitProjectOne/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitProjectOne.Startup))]
+namespace GitProjectOne
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
